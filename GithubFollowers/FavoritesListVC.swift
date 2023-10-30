a43f84c8f7108f6315d3c3d62ab10cab5b6d82d0
Enes Kaya <eng.eneskaya@gmail.com>
2023-10-30 22:56:43 +0300

    Initial Comment

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Enes Kaya on 29.10.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,45 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Enes Kaya on 29.10.2023.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    // via storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    //only configure in this class
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        //Dynamic font type
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
+
+
+
+
